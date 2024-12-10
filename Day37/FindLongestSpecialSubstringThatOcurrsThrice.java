a82e69a31f4d0b2253fc2b5bc644df5213278fa4
BKD471 <bhaskarkumardas9@gmail.com>
2024-12-11 02:26:30 +0530

    Day 37

--- Day37/FindLongestSpecialSubstringThatOcurrsThrice.java
+++ Day37/FindLongestSpecialSubstringThatOcurrsThrice.java
@@ -1,7 +1,41 @@
 package Day37;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+
+class Pair {
+    Character ch;
+    int size;
+
+    public Pair(Character ch, int size) {
+        this.ch = ch;
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return size == pair.size && Objects.equals(ch, pair.ch);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ch, size);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "ch=" + ch +
+                ", size=" + size +
+                '}';
+    }
+}
 
 public class FindLongestSpecialSubstringThatOcurrsThrice {
     // Approach 1
@@ -56,7 +90,98 @@ public class FindLongestSpecialSubstringThatOcurrsThrice {
         return maxLength;
     }
 
-    public static void main(String[] args) {
+    // Approach2
 
+    public int maximumLengthApproach2(String s) {
+        int n = s.length();
+        if (n < 3) return -1;
+
+        Map<String, Integer> hash = new HashMap<>();
+        for (int start = 0; start < n; start++) {
+            StringBuilder temp = new StringBuilder();
+            for (int end = start; end < n; end++) {
+                if (temp.isEmpty() || temp.charAt(temp.length() - 1) == s.charAt(end)) {
+                    temp.append(s.charAt(end));
+                    hash.put(temp.toString(), hash.getOrDefault(temp.toString(), 0) + 1);
+                } else break;
+            }
+        }
+
+        int maxLength = -1;
+        for (Map.Entry<String, Integer> value : hash.entrySet()) {
+            String str = value.getKey();
+            int count = value.getValue();
+            if (count >= 3 && str.length() > maxLength) maxLength = str.length();
+        }
+        return maxLength;
+    }
+
+    // Approach 3
+    public int maximumLengthApproach3(String s) {
+        int n = s.length();
+        if (n < 3) return -1;
+
+        Map<Pair, Integer> hash = new HashMap<>();
+        for (int start = 0; start < n; start++) {
+            char ch = s.charAt(start);
+            int strLength = 0;
+            for (int end = start; end < n; end++) {
+                if (ch == s.charAt(end)) {
+                    strLength++;
+                    Pair key = new Pair(ch, strLength);
+                    hash.put(key, hash.getOrDefault(key, 0) + 1);
+                } else break;
+            }
+        }
+
+        int maxLength = -1;
+        for (Map.Entry<Pair, Integer> value : hash.entrySet()) {
+            Pair key = value.getKey();
+            int count = value.getValue();
+            int size = key.size;
+            if (count >= 3 && size > maxLength) maxLength = size;
+        }
+        return maxLength;
+
+    }
+
+    // Approach4
+    public int maximumLength(String s) {
+        int n = s.length();
+        if (n < 3) return -1;
+        int[][] freq = new int[26][n + 1];
+
+        int length = 0;
+        char prev = s.charAt(0);
+
+        for (int index = 0; index < n; index++) {
+            char ch = s.charAt(index);
+            if (ch == prev) freq[ch - 'a'][++length]++;
+            else {
+                length = 1;
+                freq[ch - 'a'][length]++;
+                prev = ch;
+            }
+        }
+
+        int maxLength = -1;
+        for (int row = 0; row < 26; row++) {
+            int sum = 0;
+            for (int size = n; size >= 1; size--) {
+                sum += freq[row][size];
+                if (sum >= 3) {
+                    maxLength = Math.max(maxLength, size);
+                    break;
+                }
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String str = "aaaa";
+
+        FindLongestSpecialSubstringThatOcurrsThrice obj = new FindLongestSpecialSubstringThatOcurrsThrice();
+        System.out.println(obj.maximumLengthApproach3(str));
     }
 }
